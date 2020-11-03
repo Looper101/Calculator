71569115357d0a9ed142d586e61f27c36d8a1c91
Looper101 <hemkeys@gmail.com>
2020-11-03 09:37:28 -0600

    Added result output|Display|

--- lib/calculator_view.dart
+++ lib/calculator_view.dart
@@ -17,26 +17,47 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    final output = Provider.of<Calculator>(context, listen: true).value;
+    final inputDisplay = Provider.of<Calculator>(context, listen: true).value;
+    final result =
+        Provider.of<Calculator>(context, listen: true).evaluatedOutput;
     final prov = Provider.of<Calculator>(context, listen: false);
     return SafeArea(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-            alignment: Alignment.centerRight,
+            alignment: Alignment.bottomRight,
             height: size.height * 0.3,
             color: Colors.black,
-            child: Text(
-              output.toString(),
-              style: TextStyle(
-                  color: Colors.white, fontSize: size.longestSide * 0.1),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text(
+                    inputDisplay.toString(),
+                    style: TextStyle(
+                        color: Colors.white, fontSize: size.longestSide * 0.05),
+                  ),
+                ),
+                Spacer(),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Text(
+                    result,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: size.longestSide * 0.06,
+                        backgroundColor: Colors.red.withOpacity(0.3)),
+                  ),
+                ),
+              ],
             ),
           ),
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: Colors.grey,
+              color: Colors.black,
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -44,10 +65,23 @@ class Body extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    KeyPad(text: 'AC'),
-                    KeyPad(text: '+/-'),
-                    KeyPad(text: '%'),
-                    KeyPad(text: '/'),
+                    KeyPad(
+                      text: 'AC',
+                      onpress: () {
+                        prov.clearValue();
+                      },
+                    ),
+                    KeyPad(
+                      text: '+/-',
+                      onpress: () => prov.setValue('+'),
+                      color: Colors.red,
+                    ),
+                    KeyPad(text: '%', onpress: () => prov.setValue('%')),
+                    KeyPad(
+                      text: '/',
+                      onpress: () => prov.setValue('/'),
+                      color: Colors.red,
+                    ),
                   ],
                 ),
                 Row(
@@ -55,46 +89,89 @@ class Body extends StatelessWidget {
                   children: <Widget>[
                     KeyPad(
                       text: '7',
-                      onpress: () {
-                        prov.setValue(7);
-                      },
+                      onpress: () => prov.setValue('7'),
                     ),
                     KeyPad(
                       text: '8',
-                      onpress: () {
-                        prov.setValue(8);
-                      },
+                      onpress: () => prov.setValue('8'),
                     ),
-                    KeyPad(text: '9'),
-                    KeyPad(text: 'x'),
+                    KeyPad(
+                      text: '9',
+                      onpress: () => prov.setValue('9'),
+                    ),
+                    KeyPad(
+                      text: 'x',
+                      onpress: () => prov.setValue('*'),
+                      color: Colors.red,
+                    ),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    KeyPad(text: '4'),
-                    KeyPad(text: '5'),
-                    KeyPad(text: '6'),
-                    KeyPad(text: '-'),
+                    KeyPad(
+                      text: '4',
+                      onpress: () => prov.setValue('4'),
+                    ),
+                    KeyPad(
+                      text: '5',
+                      onpress: () => prov.setValue('5'),
+                    ),
+                    KeyPad(
+                      text: '6',
+                      onpress: () => prov.setValue('6'),
+                    ),
+                    KeyPad(
+                      text: '-',
+                      onpress: () => prov.setValue('-'),
+                      color: Colors.red,
+                    ),
                   ],
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    KeyPad(text: '1'),
-                    KeyPad(text: '2'),
-                    KeyPad(text: '3'),
-                    KeyPad(text: '+'),
+                    KeyPad(
+                      text: '1',
+                      onpress: () => prov.setValue('1'),
+                    ),
+                    KeyPad(
+                      text: '2',
+                      onpress: () => prov.setValue('2'),
+                    ),
+                    KeyPad(
+                      text: '3',
+                      onpress: () => prov.setValue('3'),
+                    ),
+                    KeyPad(
+                      text: '+',
+                      onpress: () => prov.setValue('+'),
+                      color: Colors.red,
+                    ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    KeyPad(text: '0'),
-                    KeyPad(text: '2'),
-                    KeyPad(text: '3'),
-                    KeyPad(text: '='),
-                  ],
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.2),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Expanded(
+                        child: KeyPad(
+                          text: '0',
+                          onpress: () => prov.setValue('0'),
+                        ),
+                      ),
+                      // KeyPad(
+                      //   color: Colors.red,
+                      //   text: '',
+                      //   onpress: () => prov.setValue('2'),
+                      // ),
+                      // KeyPad(
+                      //   text: '',
+                      //   onpress: () => prov.deleteValue(),
+                      // ),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -120,7 +197,8 @@ class KeyPad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return InkWell(
+    return InkResponse(
+      splashColor: Colors.red,
       onTap: onpress,
       child: Container(
         margin: EdgeInsets.symmetric(
@@ -130,7 +208,8 @@ class KeyPad extends StatelessWidget {
         width: size.width * 0.2,
         decoration: BoxDecoration(
           // shape: BoxShape.circle,
-          color: Colors.black,
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white24,
         ),
         child: Text(
           text,
--- lib/model/calculator_logic.dart
+++ lib/model/calculator_logic.dart
@@ -1,17 +1,48 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'dart:math';
 
 class Calculator extends ChangeNotifier {
-  num _value = 0;
-  num get value => _value;
+  String _value = '0';
+  String get value => _value;
+  String _evaluatedOutPut = '0';
+  String get evaluatedOutput => _evaluatedOutPut;
 
-  void setValue(int value) {
-    final StringifyedValue = value.toString();
-    if (_value == 0) {
-      _value = value;
-    } else {
-      _value = int.parse(StringifyedValue + value.toString());
-    }
+  void setValue(String newValue) {
+    Parser p = Parser();
+    String stringifiedValue;
+    notifyListeners();
+    stringifiedValue = _value = _value + newValue;
+    Expression exp = p.parse(stringifiedValue);
+    notifyListeners();
+    _evaluatedOutPut = exp.evaluate(EvaluationType.REAL, null).toString();
+    notifyListeners();
+  }
 
+  void clearValue() {
+    _value = '0';
+    _evaluatedOutPut = '0';
+    notifyListeners();
+  }
+
+  void deleteValue() {
+    _value.substring(1);
     notifyListeners();
   }
 }
+
+// final stringifyedValue = value.toString();
+// if (_value == 0) {
+//   _value = value;
+// } else {
+//   _value = int.parse(_value.toString() + newValue.toString());
+// }
+
+//.........
+// if (_value .) {
+//   _value = int.parse(_value.toString() + newValue.toString());
+// }
+
+//..................
